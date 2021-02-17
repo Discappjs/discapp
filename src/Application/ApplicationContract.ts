df82101af51788ba634364b405fc1fba9ceeccf7
Jota <jjunior@protonmail.ch>
2021-02-17 10:49:22 -0300

    Updates Application contract

--- src/Application/ApplicationContract.ts
+++ src/Application/ApplicationContract.ts
@@ -1,8 +1,11 @@
 import { Client } from 'discord.js'
 import { Signale } from 'signale'
 
+import { DiscappConfig } from '../types'
+
 export default interface ApplicationContract {
   start(): this
   getClient(): Client
   getLogger(): Signale
+  getConfig(): DiscappConfig
 }
